94b119ba00b08adb378adde94aa2671d254d773a
Xolaly <61694255+Xolaly@users.noreply.github.com>
2020-03-16 13:32:25 +0000

    added in deckSize and valueOfCard

--- blackjack_CW2/Blackjack.java
+++ blackjack_CW2/Blackjack.java
@@ -3,7 +3,8 @@ package blackjack_CW2;
 public class Blackjack {
 
 	public static void main(String[] args) {
-		System.out.println("Blackjack");
+		System.out.println("Blackjack"); 
+		System.out.println("hi");
 		System.out.println("Deck Output:");
 		
 		Deck playingDeck = new Deck();
